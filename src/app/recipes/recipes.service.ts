0bd2503ab6b54c8b0003a9e62c19e8268471acb2
Faina Kreminsky <fkreminsky@gmail.com>
2019-10-26 22:30:09 -0400

    all works so far

---
+++ src/app/recipes/recipes.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipesService {
+  private recipes: Recipe[] = [
+    {
+      id: 'r1',
+      title: 'Shnitzel',
+      imageUrl: 'https://en.wikipedia.org/wiki/Schnitzel#/media/File:Breitenlesau_Krug_Br%C3%A4u_Schnitzel.JPG',
+      ingredients: ['mashed potato', 'ketchup', 'Pork meat', 'salad']
+    },
+    {
+      id: 'r2',
+      title: 'Spaghetti',
+      imageUrl: 'https://en.wikipedia.org/wiki/Spaghetti_alla_puttanesca#/media/File:Pasta_Puttanesca.jpg',
+      ingredients: ['spaghetti', 'tomato paste', 'olives', 'parsley']
+    }
+  ];
+
+  constructor() { }
+
+  getAllRecipes() {
+    return [...this.recipes];
+  }
+
+  getRecipe(recipeId: string) {
+    return {
+      ...this.recipes.find(recipe => {
+      return recipe.id === recipeId;
+    })
+  };
+}
+}
